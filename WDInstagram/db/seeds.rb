bd4df3e891a0ab28522b201468991810e7b386c9
Toby <toby.schaeffer@gmail.com>
2015-04-14 08:13:13 -0400

    initial buildout.  update needs fixing

---
+++ WDInstagram/db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+entries = Entry.create ([
+    {author: "Toby Schaeffer", photo_url: "http://funnystack.com/wp-content/uploads/2014/04/Funny-Amazing-42.jpg"},
+    {author: "Mary Wedronek", photo_url: "http://funnystack.com/wp-content/uploads/2014/04/Funny-Amazing-42.jpg"},
+    {author: "Toby Schaeffer", photo_url: "http://www.theamazingpics.com/wp-content/uploads/2013/04/Amazing-food-art.jpg"}
+    ])
